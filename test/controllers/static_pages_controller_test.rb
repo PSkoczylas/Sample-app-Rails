00716b1d08d98182e3e839da889a0225c819cda1
PSkoczylas <piotr.skoczylas93@gmail.com>
2015-05-25 19:05:33 +0200

    Solved exercise 3.1 (Eleminate repetition) and 3.2 (add a contact page)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,5 +22,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | #{@title}"
   end
-
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@title}"
+  end
 end
