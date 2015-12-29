a5569a4571a51f2041d9eb647260dde42137eae8
PSkoczylas <piotr.skoczylas93@gmail.com>
2015-12-29 01:22:02 +0100

    Finish log in/log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,19 @@ class SessionsController < ApplicationController
   def create
     #render 'new'
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.athenticate(params[:Session][:password])
+    if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
       # log the user in and show show page
     else
-      flash.now[:danger] = 'Invalid e-mail/password compination' # it's not quite right
+      flash.now[:danger] = 'Invalid e-mail/password compination' 
       render 'new'
     end
   end
 
   def destroy
-    
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
